a9a300f68d49f06d65938063615141098180f650
Genius <g@gmail.com>
2025-03-11 17:48:29 +0530

    mes

---
+++ src/main/java/in/psa/springproject/git/SpringprojectGitApplication.java
@@ -0,0 +1,13 @@
+package in.psa.springproject.git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringprojectGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringprojectGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/psa/springproject/git/SpringprojectGitApplicationTests.java
@@ -0,0 +1,13 @@
+package in.psa.springproject.git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringprojectGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
